c7f4fa2b72895fc02a3ce67e2e6511a740ab0897
ky0613 <kyutech0613@gmail.com>
2022-03-13 16:31:01 +0900

    環境構築完了とAPIの動作確認

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -10,6 +10,8 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.api_only = true
+    config.debug_exception_response_format = :api
 
     # Configuration for the application, engines, and railties goes here.
     #
