1e545603f1d62ae11ae13fc0b059c28cb58c45e1
ky0613 <kyutech0613@gmail.com>
2022-03-21 15:12:54 +0900

    [FIX] tailwindcssの環境構築完了

---
+++ app/javascript/assets/css/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./javascript/assets/style/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
