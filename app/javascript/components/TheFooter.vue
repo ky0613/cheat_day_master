4281ca214217dd82bc7aade5e68b47370da90721
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-03-22 15:08:04 +0900

    Revert "[ADD] routerの設定と対応するファイルの作成"

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -2,13 +2,9 @@
   <footer>
     <div class="text-center p-4 bg-green-200 mx-auto">
       <ul class="flex justify-center gap-4">
-        <router-link :to="{ name: 'Terms' }"><li>利用規約</li></router-link>
-        <router-link :to="{ name: 'Policy' }"
-          ><li>プライバシーポリシー</li></router-link
-        >
-        <router-link :to="{ name: 'Contact' }"
-          ><li>お問い合わせ</li></router-link
-        >
+        <li>利用規約</li>
+        <li>プライバシーポリシー</li>
+        <li>お問い合わせ</li>
       </ul>
       <small>Copyright &copy; 2022. チートデイマスター</small>
     </div>
