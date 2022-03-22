667ab5024132afa67ba8b16869432f2a49b94e82
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-03-22 16:20:15 +0900

    Revert "Revert "[ADD] routerの設定と対応するファイルの作成""

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -3,11 +3,19 @@
     <div class="text-center">
       <div class="mb-10">
         <h3 class="mb-3">外食でチートデイを楽しみたい方はこちら</h3>
-        <button class="rounded-full bg-blue-400 p-2">外食でチートデイ</button>
+        <router-link
+          :to="{ name: 'MealOut' }"
+          class="rounded-full bg-blue-400 p-2"
+          >外食でチートデイ</router-link
+        >
       </div>
       <div>
         <h3 class="mb-3">自宅でチートデイを楽しみたい方はこちら</h3>
-        <button class="rounded-full bg-blue-400 p-2">自宅でチートデイ</button>
+        <router-link
+          :to="{ name: 'MealHome' }"
+          class="rounded-full bg-blue-400 p-2"
+          >自宅でチートデイ</router-link
+        >
       </div>
     </div>
   </div>
