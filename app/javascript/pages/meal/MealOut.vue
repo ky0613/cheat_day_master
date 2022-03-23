dd1596868431b110627336898896f9c97b8774bb
ky0613 <kyutech0613@gmail.com>
2022-03-23 23:34:25 +0900

    [ADD] 現在地取得ボタンの設置

--- app/javascript/pages/meal/MealOut.vue
+++ app/javascript/pages/meal/MealOut.vue
@@ -21,15 +21,45 @@ export default {
       version: "weekly",
     });
 
-    let map;
+    let map, infoWindow;
 
     loader.load().then(() => {
-      const google = window.google; //追加
+      const google = window.google;
       map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        zoom: 15,
       });
-      console.log(map);
+      infoWindow = new google.maps.InfoWindow();
+
+      const locationButton = document.createElement("button");
+
+      locationButton.textContent = "現在地を取得";
+      locationButton.classList.add("custom-map-control-button");
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+      locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("Location found.");
+              infoWindow.open(map);
+              map.setCenter(pos);
+            },
+            () => {
+              handleLocationError(true, infoWindow, map.getCenter());
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      });
     });
   },
 };
