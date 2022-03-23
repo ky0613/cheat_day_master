a709375970fc06508597cc4529778a968b6e2142
ky0613 <kyutech0613@gmail.com>
2022-03-23 21:19:46 +0900

    [FIX] google-maps-api-loaderライブラリの削除と公式のモジュールの適用

--- app/javascript/pages/meal/MealOut.vue
+++ app/javascript/pages/meal/MealOut.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <p>MealOut</p>
-    <TravelMap class="travel-map" />
     <router-link :to="{ name: 'MealOut' }" class="rounded-full bg-blue-400 p-2">
       ルートを検索する</router-link
     >
@@ -9,18 +8,7 @@
 </template>
 
 <script>
-import TravelMap from "../../components/TravelMap";
-
 export default {
   name: "MealOut",
-  components: {
-    TravelMap,
-  },
 };
 </script>
-
-<style lang="scss" scoped>
-.travel-map {
-  height: 400px;
-}
-</style>
