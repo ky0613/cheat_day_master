3f228a3df2783134897bf6db81a4883d32d7e761
ky0613 <kyutech0613@gmail.com>
2022-03-24 16:15:42 +0900

    [ADD] 検索ボックスの作成

--- app/javascript/components/GoogleMap.vue
+++ app/javascript/components/GoogleMap.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="map_wrapper">
     <div id="map" class="map"></div>
+    <input
+      id="pac-input"
+      class="%i[controls ignore-enterkey]"
+      type="text"
+      placeholder="検索"
+    />
   </div>
 </template>
 
@@ -12,67 +18,227 @@ export default {
     const loader = new Loader({
       apiKey: "AIzaSyBe69s9_xYPZtwWPCUykqCP2qpimoGwfwg",
       version: "weekly",
+      libraries: ["places"],
     });
 
-    let map, infoWindow;
+    let map;
 
     loader.load().then(() => {
       const google = window.google;
       map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 35.6581, lng: 139.7017 },
-        zoom: 18,
+        zoom: 16,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false,
       });
-      infoWindow = new google.maps.InfoWindow();
 
+      ////////////////// 現在地ボタン作成 //////////////////
       const locationButton = document.createElement("button");
-
       locationButton.textContent = "現在地を取得";
+      // 現在地ボタンのCSS
       Object.assign(locationButton.style, {
-        backgroundColor: "#fff",
-        border: "0",
+        appearance: "button",
+        backgroundColor: "dimgray",
+        border: "1px solid black",
         borderRadius: "2px",
         boxShadow: "0 1px 4px -1px rgb(0 0 0 / 30%)",
         margin: "10px",
-        padding: "0 0.5em",
-        font: "400 18px Roboto, Arial, sans-serif",
+        padding: "3px 11px 3px 13px",
+        font: "400 15px Roboto, Arial, sans-serif",
+        color: "white",
         overflow: "hidden",
-        height: "40px",
+        height: "30px",
         cursor: "pointer",
       });
-      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+      locationButton.addEventListener("mouseover", () => {
+        Object.assign(locationButton.style, {
+          backgroundColor: "white",
+          color: "black",
+        });
+      });
+      locationButton.addEventListener("mouseleave", () => {
+        Object.assign(locationButton.style, {
+          backgroundColor: "dimgray",
+          color: "white",
+        });
+      });
+      map.controls[google.maps.ControlPosition.TOP_RIGHT].push(locationButton);
+
+      // 位置情報ボタンのクリックイベント
       locationButton.addEventListener("click", () => {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              };
-
-              infoWindow.setPosition(pos);
-              infoWindow.setContent("現在地");
-              infoWindow.open(map);
-              map.setCenter(pos);
-            },
-            () => {
-              handleLocationError(true, infoWindow, map.getCenter());
-            }
-          );
+          navigator.geolocation.getCurrentPosition((position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            map.setCenter(pos);
+          });
         } else {
-          handleLocationError(false, infoWindow, map.getCenter());
+          return false;
         }
       });
-    });
+      ////////////////////////////////////////////////////
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(
-        browserHasGeolocation
-          ? "Error: The Geolocation service failed."
-          : "Error: Your browser doesn't support geolocation."
-      );
-      infoWindow.open(map);
-    }
+      ////////////////// 検索ボックス作成 //////////////////
+      const input = document.getElementById("pac-input");
+      const searchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      // 検索結果で表示範囲変更する
+      map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
+      });
+      let markers = [];
+      // 検索で発火
+      searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+        if (places.length == 0) {
+          return;
+        }
+        // 古いマーカー削除
+        markers.forEach((marker) => {
+          marker.setMap(null);
+        });
+        markers = [];
+        const bounds = new google.maps.LatLngBounds();
+        for (let i = 0; i < places.length; i++) {
+          if (!places[i].geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+          }
+          // マーカー作成
+          let marker = new google.maps.Marker({
+            map,
+            title: places[i].name, // 名称
+            position: places[i].geometry.location, // 緯度軽度
+            address: places[i].formatted_address, // 住所
+          });
+          // infowindow表示内容
+          const contentString =
+            `<div id="ababab">` +
+            `<p>${marker.title}</p>` +
+            `<p>${marker.address}</p>` +
+            `<input type="button" value="現在地に設定" id="addStartPoint"  href="#startPoint">` +
+            `<input type="button" value="行きたいお店に設定" id="addDestination" href="#destination">` +
+            `</div>`;
+          markers.push(new google.maps.Marker({ marker }));
+          attachInfoWindow(marker, places, contentString, i);
+
+          if (places[i].geometry.viewport) {
+            bounds.union(places[i].geometry.viewport);
+          } else {
+            bounds.extend(places[i].geometry.location);
+          }
+        }
+        map.fitBounds(bounds);
+      });
+
+      // Infowindowを１つのみ開くために設定
+      let currentInfoWindow = null;
+      // マップ上のデフォルトで表示されるアイコンをクリックすると発火
+      map.addListener("click", function (argument) {
+        if (argument.placeId) {
+          let placeId = argument.placeId;
+          let service = new google.maps.places.PlacesService(map);
+          service.getDetails(
+            {
+              placeId: placeId,
+              fields: ["name", "formatted_address", "geometry"],
+            },
+            function (placeOnMap, status) {
+              if (status == google.maps.places.PlacesServiceStatus.OK) {
+                let contentOnMap =
+                  `<div id="ababab">` +
+                  `<p>${placeOnMap.name}</p>` +
+                  `<p>${placeOnMap.formatted_address}</p>` +
+                  `<input type="button" value="出発地点に設定" id="addStartPoint" href="#startPoint">` +
+                  `<input type="button" value="待ち合わせ場所に設定" id="addDestination" href="#destination">` +
+                  `</div>`;
+                let infowindow = new google.maps.InfoWindow({
+                  content: contentOnMap,
+                  position: argument.latLng,
+                });
+                if (currentInfoWindow) {
+                  currentInfoWindow.close();
+                }
+                infowindow.open(map);
+                currentInfoWindow = infowindow;
+                // HTMLのformに値を送信
+                infowindow.addListener("domready", () => {
+                  document
+                    .getElementById("addStartPoint")
+                    .addEventListener("click", () => {
+                      document.getElementById("data-start-point-name").value =
+                        placeOnMap.name;
+                      document.getElementById(
+                        "data-start-point-location"
+                      ).value = placeOnMap.geometry.location;
+                      document.getElementById(
+                        "data-start-point-address"
+                      ).value = placeOnMap.formatted_address;
+                    });
+                  document
+                    .getElementById("addDestination")
+                    .addEventListener("click", () => {
+                      document.getElementById("data-destination-name").value =
+                        placeOnMap.name;
+                      document.getElementById(
+                        "data-destination-location"
+                      ).value = placeOnMap.geometry.location;
+                      document.getElementById(
+                        "data-destination-address"
+                      ).value = placeOnMap.formatted_address;
+                    });
+                });
+              }
+            }
+          );
+        }
+      });
+      // マップ上クリックでinfowindow閉じる
+      google.maps.event.addListener(map, "click", function () {
+        if (currentInfoWindow) {
+          currentInfoWindow.close();
+        }
+      });
+      // 検索結果のマーカークリックで発火
+      function attachInfoWindow(marker, places, contentString) {
+        let infowindow = new google.maps.InfoWindow({
+          content: contentString,
+        });
+        marker.addListener("click", () => {
+          if (currentInfoWindow) {
+            currentInfoWindow.close();
+          }
+          infowindow.open(marker.get("map"), marker);
+          currentInfoWindow = infowindow;
+          // HTMLのformに値を送信
+          infowindow.addListener("domready", () => {
+            document
+              .getElementById("addStartPoint")
+              .addEventListener("click", () => {
+                document.getElementById("data-start-point-name").value =
+                  marker.title;
+                document.getElementById("data-start-point-location").value =
+                  marker.position;
+                document.getElementById("data-start-point-address").value =
+                  marker.address;
+              });
+            document
+              .getElementById("addDestination")
+              .addEventListener("click", () => {
+                document.getElementById("data-destination-name").value =
+                  marker.title;
+                document.getElementById("data-destination-location").value =
+                  marker.position;
+                document.getElementById("data-destination-address").value =
+                  marker.address;
+              });
+          });
+        });
+      }
+    });
   },
 };
 </script>
@@ -90,5 +256,31 @@ export default {
   height: 100%;
   top: 0;
   left: 0;
+}
+
+#pac-input {
+  background-color: #fff;
+  font-size: 15px;
+  font-weight: 300;
+  margin: 10px 0 0 10px;
+  padding: 3px 8px 3px 13px;
+  text-overflow: ellipsis;
+  width: 230px;
+  height: 30px;
+}
+
+#pac-input:focus {
+  border-color: #4d90fe;
+}
+
+.pac-controls {
+  display: inline-block;
+  padding: 5px 11px;
+}
+
+.pac-controls label {
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 300;
 }
 </style>
