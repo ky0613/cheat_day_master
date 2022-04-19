e9a92625db95683a1da2d35d6edc1f1500b99970
ky0613 <kyutech0613@gmail.com>
2022-04-19 13:27:25 +0900

    [FIX] RakutenApiClientをリファクタリング

--- lib/rakuten_api_client.rb
+++ lib/rakuten_api_client.rb
@@ -1,25 +1,34 @@
 class RakutenApiClient
 
-  class << self
-
     RAKUTEN_HOST = "https://app.rakuten.co.jp/services"
     GET_URI = "#{RAKUTEN_HOST}/api/IchibaItem/Ranking/20170628?"
+
+    def initialize
+      @token = Rails.application.credentials.rakuten[:token]
+      @genre_id = Rails.application.credentials.rakuten[:genre_id]
+    end
 
     def get_items
       params = URI.encode_www_form({
         format: "json",
-        genreId: Rails.application.credentials.rakuten[:genre_id],
-        applicationId: Rails.application.credentials.rakuten[:token],
+        genreId: @genre_id,
+        applicationId: @token,
       })
       uri = URI.parse("#{GET_URI}#{params}")
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      request = Net::HTTP::Get.new(
-        uri.request_uri
-      )
+      request = Net::HTTP::Get.new(uri.request_uri)
       response = http.request(request)
       JSON.parse(response.body)
     end
 
-  end
+    class << self
+      def client
+        RakutenApiClient.new
+      end
+
+      def get_items
+        client.get_items
+      end
+    end
 end
