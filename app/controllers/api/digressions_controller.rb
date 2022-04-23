013b4c39fd7a475473a7810de614ef20a1aefa24
ky0613 <kyutech0613@gmail.com>
2022-04-24 03:56:06 +0900

    [ADD] コントローラー内でjsonを返す処理を追加

--- app/controllers/api/digressions_controller.rb
+++ app/controllers/api/digressions_controller.rb
@@ -1,4 +1,7 @@
 class Api::DigressionsController < ApplicationController
   def index
+    array = Digression.all
+    @digressions = array.shuffle.slice(0, 4)
+    render json: @digressions
   end
 end
