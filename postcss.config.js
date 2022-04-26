37e12462c1751a765f8ea4414b5a6cac7dd36bf1
ky0613 <kyutech0613@gmail.com>
2022-04-26 19:36:50 +0900

    [FIX] postcssの設定を変更

--- postcss.config.js
+++ postcss.config.js
@@ -11,3 +11,17 @@ module.exports = {
     }),
   ],
 };
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        "./app/**/*.html.erb",
+        "./app/**/*.js.erb",
+        "./app/helpers/**/*.rb",
+      ],
+      safelist: ["a", "open"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    })
+  );
+}
