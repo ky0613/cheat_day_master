c1f674f2706d6e31b0172ff56bef334ff7069ed7
ky0613 <kyutech0613@gmail.com>
2022-04-27 21:07:38 +0900

    [ADD] コントローラーに処理を追加，モジュールを作成

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::UsersController < ApplicationController
+  before_action :authenticate!, only: %i[me]
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+
+  def me
+    render json: current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
 end
