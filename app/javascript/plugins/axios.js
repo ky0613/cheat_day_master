6295f7ae28472cd1b611bd69b56b08ddeed70c4e
ky0613 <kyutech0613@gmail.com>
2022-04-27 18:07:19 +0900

    [FIX] axiosの設定を変更

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const production = "https://cheat-day-master.herokuapp.com";
-const development = "http://localhost:5000";
-const url = process.env.NODE_ENV ? production : development;
+const development = "http://127.0.0.1:5000";
+const url = process.env.NODE_ENV === "production" ? production : development;
 
 const axiosConfig = axios.create({ baseURL: `${url}/api/` });
 export default axiosConfig;
