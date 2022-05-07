b2de0325e586f25e825b0be99d65e1eb2412cdbc
ky0613 <kyutech0613@gmail.com>
2022-05-07 16:51:14 +0900

    [ADD] 店舗保存用のaxiosの処理を追加

---
+++ app/javascript/store/modules/stores.js
@@ -0,0 +1,38 @@
+import axios from "../../plugins/axios";
+
+const state = {
+  stores: [],
+};
+
+const getters = {
+  savedStores: (state) => state.stores,
+};
+
+const actions = {
+  async fetchStores({ commit }) {
+    const response = await axios.get("stores");
+    commit("setStores", response.data);
+  },
+  async addStore({ commit }, store) {
+    const response = await axios.post("stores", store);
+    commit("newStore", response.data);
+  },
+  async deleteStore({ commit }, store_id) {
+    await axios.delete(`stores/${id}`);
+    commit("removeStore", id);
+  },
+};
+
+const mutations = {
+  setStore: (state, stores) => (state.stores = stores),
+  newStore: (state, store) => state.stores.push(store),
+  removeStore: (state, id) =>
+    (state.stores = state.stores.filter((store) => store.id !== id)),
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
