bf737034f467c986858b33aaa1252ab70043bbb9
ky0613 <kyutech0613@gmail.com>
2022-05-09 09:03:05 +0900

    [FIX] パスワード入力時のバリデーションのタイミングを変更

--- app/javascript/store/modules/googleMealHomeStores.js
+++ app/javascript/store/modules/googleMealHomeStores.js
@@ -1,3 +1,20 @@
+const convertStoreData = (stores) => {
+  let googleStores = [];
+  stores.map((store) => {
+    let convertStore = {
+      store_id: store.place_id,
+      img_url: store.storePhoto,
+      name: store.name,
+      address: store.vicinity,
+      rating: store.rating,
+      total_ratings: store.user_ratings_total,
+      store_type: "Google",
+    };
+    googleStores.push(convertStore);
+  });
+  return googleStores;
+};
+
 export default {
   namespaced: true,
   state: {
@@ -18,7 +35,9 @@ export default {
       });
       commit(
         "setDeliveryStores",
-        notHotelPositions.sort((a, b) => (a.rating < b.rating ? 1 : -1))
+        convertStoreData(notHotelPositions).sort((a, b) =>
+          a.rating < b.rating ? 1 : -1
+        )
       );
     },
   },
