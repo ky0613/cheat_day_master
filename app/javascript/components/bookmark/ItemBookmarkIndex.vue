ddeb678bb08af5a7084514fc61d76e5b5840fc9f
ky0613 <kyutech0613@gmail.com>
2022-05-10 12:26:49 +0900

    [ADD] ブックマークした店舗の表示をコンポーネント化

---
+++ app/javascript/components/bookmark/ItemBookmarkIndex.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex flex-col mt-5">
+    <div class="w-full grid grid-cols-3 gap-4">
+      <div v-for="item in items" :key="item.id">
+        <div
+          class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full"
+        >
+          <div class="w-full">
+            <a
+              class="text-sm text-gray-700 dark:text-gray-400 line-clamp-3 hover:text-blue-400 hover:underline"
+              :href="item.item_url"
+              target="_blank"
+            >
+              {{ item.name }}
+            </a>
+            <hr />
+          </div>
+          <div class="p-2 flex">
+            <img class="mx-1" :src="item.img_url" />
+            <div
+              class="flex flex-col mt-4 text-gray-700 dark:text-gray-200 justify-right"
+            >
+              <div class="flex mb-3">
+                <img src="../../../../public/money.svg" class="w-5 h-5" />
+                <p class="px-2 text-base font-semibold">{{ item.price }}円</p>
+              </div>
+              <div class="flex">
+                <img src="../../../../public/shop.svg" class="w-5 h-5" />
+                <p class="px-2 text-sm">{{ item.shop_name }}</p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="flex items-center mt-4 text-gray-700 dark:text-gray-200 pb-4"
+          >
+            <StarRating
+              :increment="0.01"
+              :read-only="true"
+              :rating="Number(item.rating)"
+              :star-size="30"
+              class="items-center text-base"
+            />
+            <p class="px-2 text-base items-center">
+              ({{ item.total_ratings }})
+            </p>
+            <ItemBookmark :item="item" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import StarRating from "vue-star-rating";
+import ItemBookmark from "../../components/ItemBookmark.vue";
+
+export default {
+  components: {
+    StarRating,
+    ItemBookmark,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ app/javascript/components/bookmark/StoreBookmarkIndex.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flex flex-col mt-5">
+    <div class="w-full grid grid-cols-3 gap-4" v-if="stores">
+      <div v-for="store in stores" :key="store.id">
+        <div
+          class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full"
+        >
+          <a :href="storeUrl(store)" target="_blank">
+            <img
+              class="object-cover object-center w-full h-56"
+              :src="store.img_url"
+              alt="avatar"
+            />
+          </a>
+          <div class="p-2">
+            <div
+              class="flex items-center mt-4 text-gray-700 dark:text-gray-200 h-10"
+            >
+              <img src="../../../../public/shop.svg" class="w-5 h-5" />
+              <p class="px-2 text-l font-semibold text-center">
+                {{ store.name }}
+              </p>
+            </div>
+            <div
+              class="flex items-center mt-4 text-gray-700 dark:text-gray-200 h-6"
+            >
+              <img src="../../../../public/place.svg" class="w-5 h-5" />
+              <p class="px-2 text-sm">{{ store.address }}</p>
+            </div>
+            <div
+              class="flex items-center mt-4 text-gray-700 dark:text-gray-200"
+            >
+              <StarRating
+                :increment="0.1"
+                :round-start-rating="false"
+                :read-only="true"
+                :rating="store.rating"
+                :star-size="25"
+                class="items-center text-base"
+              ></StarRating>
+              <p class="px-2 text-base items-center jus">
+                ({{ store.total_ratings }})
+              </p>
+              <StoreBookmark :store="store" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import StarRating from "vue-star-rating";
+import StoreBookmark from "../../components/StoreBookmark.vue";
+
+export default {
+  components: {
+    StarRating,
+    StoreBookmark,
+  },
+  props: {
+    stores: {
+      type: Array,
+    },
+  },
+  methods: {
+    storeUrl(store) {
+      if (store.store_url) {
+        return store.store_url;
+      }
+      return `https://maps.google.co.jp/maps?q=place_id:${store.store_id}`;
+    },
+  },
+};
+</script>
