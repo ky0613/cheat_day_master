fa4763c11a86db2a593177dedb22a5a4a4fb124d
ky0613 <kyutech0613@gmail.com>
2022-05-12 23:29:22 +0900

    [ADD] twitter認証用にmigration作成とモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   include JwtToken
   authenticates_with_sorcery!
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
+
   has_many :stores, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :recipes, dependent: :destroy
@@ -9,6 +12,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true
   validates :name, presence: true
 end
--- db/migrate/20220427114040_sorcery_core.rb
+++ db/migrate/20220427114040_sorcery_core.rb
@@ -2,7 +2,7 @@ class SorceryCore < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null:false
-      t.string :email,            null: false, index: { unique: true }
+      t.string :email, index: { unique: true }
       t.string :crypted_password
       t.string :salt
 
