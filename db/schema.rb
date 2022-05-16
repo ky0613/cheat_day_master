7ed9133e8a59a95e7e8e90591e155501e330b24f
ky0613 <kyutech0613@gmail.com>
2022-05-16 22:21:10 +0900

    [ADD] パスワードリセット機能追加のためのカラム追加と設定変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_031144) do
+ActiveRecord::Schema.define(version: 2022_05_16_115721) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -97,6 +97,11 @@ ActiveRecord::Schema.define(version: 2022_05_16_031144) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "twitter_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "items", "users"
