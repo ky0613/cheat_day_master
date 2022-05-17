10143d36e794b771b2c131459ad1258680344f23
ky0613 <kyutech0613@gmail.com>
2022-05-17 12:59:37 +0900

    [ADD] リセットパスワードのメール内容の作成とコードの修正

--- app/controllers/api/password_resets_controller.rb
+++ app/controllers/api/password_resets_controller.rb
@@ -1,35 +1,27 @@
 class Api::PasswordResetsController < ApplicationController
+
   def create
-    @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
-    redirect_to(root_path, :notice => 'メールアドレス宛にメールを送信しました。')
-  end
-
-  def edit
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(@token)
-
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    user = User.find_by_email(params[:email])
+    user&.generate_reset_password_token!
+    UserMailer.reset_password_email(user).deliver_now if user
   end
 
   def update
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(@token)
+    user = User.load_from_reset_password_token(params[:token])
+    raise ActiveRecord::RecordNotFound unless user
 
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    user.password_confirmation = user_params[:password_confirmation]
 
-    @user.password_confirmation = params[:user][:password_confirmation]
-
-    if @user.change_password(params[:user][:password])
-      redirect_to(root_path, :notice => 'パスワードを更新しました')
+    if user.change_password(user_params[:password])
+      head :ok
     else
-      render :action => "edit"
+      render json: { errors: user.errors }, status: :bad_request
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
