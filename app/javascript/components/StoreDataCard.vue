2ced894815b311abd0a64967d8a0011584b0def3
ky0613 <kyutech0613@gmail.com>
2022-05-22 03:33:28 +0900

    [ADD] 結果画面全体のレスポンシブ対応

--- app/javascript/components/StoreDataCard.vue
+++ app/javascript/components/StoreDataCard.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
-    <Carousel :perPage="this.perPage" :paginationPadding="4">
+    <Carousel
+      :perPage="carouselPage"
+      :paginationPadding="3"
+      paginationPosition="bottom-overlay"
+      class="pb-6"
+    >
       <Slide
         v-for="(store, index) of stores"
         :key="store.store_id"
-        class="px-2 my-3"
+        class="md:px-2 md:my-3 md:w-full max-w-1/2 px-1 h-full"
       >
         <div
-          class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full border"
+          class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full h-full border"
         >
           <a
             :href="`https://maps.google.co.jp/maps?q=place_id:${store.store_id}`"
             target="_blank"
           >
             <img
-              class="object-cover object-center w-full h-56"
+              class="object-cover object-center w-full md:h-56 h-32"
               :src="store.img_url"
               alt="avatar"
             />
           </a>
           <div class="p-2">
-            <div v-if="wayPoint" class="text-l font-semibold text-center">
+            <div
+              v-if="wayPoint"
+              class="md:text-l text-xs font-semibold text-center"
+            >
               {{ index + 1 }}店舗目
             </div>
             <div
               class="flex items-center mt-4 text-gray-700 dark:text-gray-200 h-10"
             >
               <img src="../../../public/img/shop.svg" class="w-5 h-5" />
-              <p class="px-2 text-l font-semibold text-center">
+              <p class="px-2 md:text-lg text-xs font-semibold text-center">
                 {{ store.name }}
               </p>
             </div>
@@ -35,20 +43,20 @@
               class="flex items-center mt-4 text-gray-700 dark:text-gray-200 h-6"
             >
               <img src="../../../public/img/place.svg" class="w-5 h-5" />
-              <p class="px-2 text-sm">{{ store.address }}</p>
+              <p class="px-2 md:text-sm text-xs">{{ store.address }}</p>
             </div>
             <div
               class="flex items-center mt-4 text-gray-700 dark:text-gray-200"
             >
               <StarRating
-                :increment="0.1"
+                :increment="0.01"
                 :round-start-rating="false"
                 :read-only="true"
                 :rating="store.rating"
-                :star-size="25"
-                class="items-center text-base"
-              ></StarRating>
-              <p class="px-2 text-base items-center jus">
+                :star-size="starSize"
+                text-class="md:text-base text-xs mt-1"
+              />
+              <p class="md:text-base text-xs mt-1">
                 ({{ store.total_ratings }})
               </p>
               <StoreBookmark :store="store" />
@@ -83,6 +91,14 @@ export default {
     wayPoint: {
       type: Boolean,
       default: false,
+    },
+  },
+  computed: {
+    carouselPage() {
+      return window.innerWidth >= 768 ? this.perPage : 2;
+    },
+    starSize() {
+      return window.innerWidth >= 768 ? 20 : 15;
     },
   },
 };
--- app/javascript/components/mealout/HotPepperGourmandStores.vue
+++ app/javascript/components/mealout/HotPepperGourmandStores.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <Carousel :perPage="3" :paginationPadding="4">
-      <Slide v-for="store of stores" :key="store.store_id" class="px-2 my-3">
+    <Carousel
+      :perPage="carouselPage"
+      :paginationPadding="3"
+      paginationPosition="bottom-overlay"
+      class="pb-6"
+    >
+      <Slide
+        v-for="store of stores"
+        :key="store.store_id"
+        class="md:px-2 md:my-3 md:w-full max-w-1/2 px-1 h-full"
+      >
         <div
-          class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full flex flex-col border"
+          class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full flex flex-col border h-full"
         >
           <a class="bg-blue-300 w-full" :href="store.store_url" target="_blank">
             <img
-              class="object-cover object-center w-full h-56"
+              class="object-cover object-center w-full md:h-56 h-32"
               :src="store.img_url"
               alt="avatar"
             />
@@ -17,7 +26,7 @@
               class="flex items-center mt-4 text-gray-700 dark:text-gray-200 h-10"
             >
               <img src="../../../../public/img/shop.svg" class="w-5 h-5" />
-              <p class="px-2 text-l font-semibold text-center">
+              <p class="px-2 md:text-lg text-xs font-semibold text-center">
                 {{ store.name }}
               </p>
             </div>
@@ -25,7 +34,7 @@
               class="flex items-center mt-4 text-gray-700 dark:text-gray-200 h-6"
             >
               <img src="../../../../public/img/place.svg" class="w-5 h-5" />
-              <p class="px-2 text-sm">{{ store.address }}</p>
+              <p class="px-2 md:text-sm text-xs">{{ store.address }}</p>
             </div>
           </div>
           <div class="flex">
@@ -64,6 +73,14 @@ export default {
     stores: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    carouselPage() {
+      return window.innerWidth >= 768 ? 3 : 2;
+    },
+    starSize() {
+      return window.innerWidth >= 768 ? 20 : 15;
     },
   },
 };
