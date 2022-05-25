cefa54f2c4f4364f31de9bb2b0a63a3707659ef1
ky0613 <kyutech0613@gmail.com>
2022-05-25 21:40:19 +0900

    [ADD] RSpecの設定を変更と必要なファイルの作成

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module CheatDayMaster
       g.test_framework :rspec,
                         view_specs: false,
                         helper_specs: false
+                        controller_specs: false,
+                        routing_specs: false
     end
 
     # Configuration for the application, engines, and railties goes here.
