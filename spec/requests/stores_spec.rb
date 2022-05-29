dd02043115dbb225e64f28bcbb3ab869bca4449e
ky0613 <kyutech0613@gmail.com>
2022-05-29 23:52:12 +0900

    [ADD] CRUDのテストを追加

--- spec/requests/stores_spec.rb
+++ spec/requests/stores_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe "Stores", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /api/stores" do
+    it "storeを追加できる" do
+      post api_stores_path(params: store)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "DELETE /api/stores/:id" do
+    it "storeを削除できる" do
+      delete api_stores_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
