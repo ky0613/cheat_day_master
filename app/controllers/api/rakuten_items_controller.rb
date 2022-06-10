b1307108cd8b885d140626fd251d44bc83488926
ky0613 <kyutech0613@gmail.com>
2022-06-10 09:23:07 +0900

    [ADD] 楽天API用のGem追加に伴う処理の変更

--- app/controllers/api/rakuten_items_controller.rb
+++ app/controllers/api/rakuten_items_controller.rb
@@ -1,6 +1,6 @@
 class Api::RakutenItemsController < ApplicationController
   def index
-    response_json = RakutenApiClient.get_items(params[:food_genre_id])
+    response_json = RakutenWebService::Ichiba::Genre[params[:food_genre_id]].ranking.each {|ranking| ranking}
     render json: response_json
   end
 end
